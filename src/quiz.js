4c9eef1b72855e993c0d99e350bc4af4def85868
Alba Perez <albaperezbaldo@gmail.com>
2025-04-30 16:22:44 +0200

    lab

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,63 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        return this.currentQuestionIndex += 1;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = this.questions.length -1; i > 0; i--){
+            const j = Math.floor(Math.random()*(i+1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]]; 
+        }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
 
-    // 6. hasEnded()
+        const trueQuestion = this.questions[this.currentQuestionIndex];
+        if ( this.answer === trueQuestion.correctAnswers) {
+            this.correctAnswers +=1;
+            return true;
+        }
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        }
+        else if (this.currentQuestionIndex = this.questions.length) {
+            return true;
+        }
+    }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if ( difficulty >= 1 && difficulty <= 3) {
+            this.questions = this.questions.filter(question => question.difficulty === difficulty);
+        }
+    } // lo que hace el condicional es que si se cumple la condición, this.question pasa a "filtrar" la dificultad
+    // asignada a cada una de las preguntas, y solo quedará en this.question las que cumplan con la dificultad que propone el código
+    // es decir de 1 a 3.
+
+    averageDifficulty() {
+        //should return the average difficulty (number) of the questions in the quiz
+        const totaldificultad = this.questions.reduce((sum, question) => sum + question.difficulty, 0);
+        // esto suma todas las dificultades de las preguntas en la variable totaldificultad, 
+        //con el .reduce recorremos el array y acumula el valor de question.difficulty
+        return totaldificultad / this.questions.length;
+        //divide la suma de dificultades entre la cantidad de preguntas y nos da como resultado el rpomedio de dificultad
+    }
+
+
 }
