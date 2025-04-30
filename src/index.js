ee0ba2942966b2fc997eec391d7b10ea675dfd79
Gaelle <gaelle.madelaine@gmail.com>
2025-04-30 18:36:22 +0200

    Dia 4 - hasta el 3

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   // Create a new Quiz instance object
   const quiz = new Quiz(questions, quizDuration, quizDuration);
+  console.log(quiz)
   // Shuffle the quiz questions
   quiz.shuffleQuestions();
 
@@ -89,6 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Get the current question from the quiz by calling the Quiz class method `getQuestion()`
     const question = quiz.getQuestion();
+    console.log(question)
     // Shuffle the choices of the current question by calling the method 'shuffleChoices()' on the question object
     question.shuffleChoices();
     
@@ -98,20 +100,22 @@ document.addEventListener("DOMContentLoaded", () => {
     //
     // 1. Show the question
     // Update the inner text of the question container element and show the question text
+    questionContainer.innerText = question.text
 
+    
     
     // 2. Update the green progress bar
     // Update the green progress bar (div#progressBar) width so that it shows the percentage of questions answered
     
-    progressBar.style.width = `65%`; // This value is hardcoded as a placeholder
+    progressBar.style.width = `${(quiz.currentQuestionIndex + 1) / (quiz.questions.length) * 100}%` ; // This value is hardcoded as a placeholder
 
 
 
     // 3. Update the question count text 
     // Update the question count (div#questionCount) show the current question out of total questions
     
-    questionCount.innerText = `Question 1 of 10`; //  This value is hardcoded as a placeholder
-
+    questionCount.innerText = `Question ${quiz.currentQuestionIndex + 1} of ${quiz.questions.length}`; //  This value is hardcoded as a placeholder
+    
 
     
     // 4. Create and display new radio input element with a label for each choice.
@@ -127,7 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
       // Hint 2: You can use the `element.type`, `element.name`, and `element.value` properties to set the type, name, and value of an element.
       // Hint 3: You can use the `element.appendChild()` method to append an element to the choices container.
       // Hint 4: You can use the `element.innerText` property to set the inner text of an element.
-
+       
   }
 
 
@@ -138,6 +142,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     // YOUR CODE HERE:
+      
+
     //
     // 1. Get all the choice elements. You can use the `document.querySelectorAll()` method.
 
